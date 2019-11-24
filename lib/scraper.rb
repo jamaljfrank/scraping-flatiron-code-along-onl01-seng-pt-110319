aff8835f14c8677c8ab24fb9c553f4015fc11bcd
Jamal Frank <jamaljfrank@gmail.com>
2019-11-24 04:39:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
   end
   
   def get_courses
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    get_page
     doc.css("h2").text
   end
   
